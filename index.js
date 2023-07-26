9689fe1ebff226fa18733bc33a1eb2378d51ce85
faize700 <114169171+faize700@users.noreply.github.com>
2023-07-26 23:41:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const btnEle = document.getElementById("btn");
+const jokeEle = document.getElementById("jokes");
+
+const apiKey = "Ld1+tD5lDdfAh5quyprDhg==W2Dtgqqk7TW9D7ez";
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+option = {
+    method : "GET",
+    headers : {
+        "X-Api-Key" : apiKey
+    }
+}
+
+async function getJoke(){
+
+    try {
+        jokeEle.innerText = "Updating...";
+        btnEle.disabled = true;
+        btnEle.innerText = "Loading";
+        
+        const response = await fetch(apiUrl,option);
+        const data = await response.json();
+
+        btnEle.disabled = false;
+        btnEle.innerText = "Tell me a Joke";
+        jokeEle.innerText = data[0].joke;
+    } catch (error) {
+        jokeEle.innerText = "An Error happened, check and try again!";
+        btnEle.disabled = false;
+        btnEle.innerText = "Tell me a Joke";
+        console.log('Error:', error);
+    }
+
+    
+}
+
+btnEle.addEventListener("click", getJoke);
