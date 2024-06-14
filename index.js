3aa2a908b1b5c89e5b61281180fda506ed530df3
Gul Muazzam Faize <corpgmfaize@gmail.com>
2024-06-14 18:06:50 +0530

    Update index.js
    API Update

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const btnEle = document.getElementById("btn");
 const jokeEle = document.getElementById("jokes");
 
 const apiKey = "Ld1+tD5lDdfAh5quyprDhg==W2Dtgqqk7TW9D7ez";
-const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+// const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const apiUrl = "https://icanhazdadjoke.com/";
 
 option = {
     method : "GET",
-    headers : {
-        "X-Api-Key" : apiKey
-    }
+    // headers : {
+    //     "X-Api-Key" : apiKey
+    // }
+    headers:{
+            accept :" application/json" ,
+        },
 }
 
 async function getJoke(){
@@ -23,7 +27,7 @@ async function getJoke(){
 
         btnEle.disabled = false;
         btnEle.innerText = "Tell me a Joke";
-        jokeEle.innerText = data[0].joke;
+        jokeEle.innerText = data.joke;
     } catch (error) {
         jokeEle.innerText = "An Error happened, check and try again!";
         btnEle.disabled = false;
